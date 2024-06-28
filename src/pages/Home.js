947df29ff45821a7dccd6d58eaa2b2b718e7867b
Girija Sankar Dash <etfdukaan@gmail.com>
2024-06-28 11:45:03 +0530

    Auto-commit: 28/06/2024 11:45:03.25

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -28,17 +28,4 @@ const Home = () => {
   );
 };
 
-// export default8. **src/pages/JobPage.js**
-// ```jsx
-// import React from 'react';
-// import JobDetail from '../components/JobDetail';
-
-// const JobPage = ({ match }) => {
-//   return (
-//     <div>
-//       <JobDetail match={match} />
-//     </div>
-//   );
-// };
-
-// export default JobPage;
+export default Home;
